6c2765ae749d7a47cbebb635572a303583266da6
KS_ABBAS <145643357+KSABBAS@users.noreply.github.com>
2024-10-04 00:37:27 +0300

    sign up is added

--- codeveloper_to_do/lib/auth/Login/LogInView.dart
+++ codeveloper_to_do/lib/auth/Login/LogInView.dart
@@ -3,7 +3,6 @@ import 'package:codeveloper_to_do/MyTools.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class Loginview extends StatelessWidget {
   const Loginview({super.key});
@@ -12,162 +11,198 @@ class Loginview extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
     return Scaffold(
-      backgroundColor: UsedColors.Blue,
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            flex: 20,
-            child: CMaker(
-                alignment: Alignment.center,
-                child: TMaker(
-                    text: "Log In",
-                    fontSize: 40,
-                    fontWeight: FontWeight.w600,
-                    color: UsedColors.White)),
-          ),
-          CMaker(
-            color: UsedColors.White,
-            circularRadius: 20,
-            height: 400,
+          Container(
+            height: double.infinity,
             width: double.infinity,
-            boxShadow: [
-              BoxShadow(
-                  blurRadius: 10,
-                  color: const Color.fromARGB(91, 0, 0, 0),
-                  offset: Offset(2, 2),
-                  spreadRadius: .3)
-            ],
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            padding: EdgeInsets.all(30),
-            child: Column(
-              children: [
-                PMaker(
-                  top: 20,
-                ),
-                TextField(
-                  onChanged: (value) {
-                    print(value);
-                  },
-                  decoration: InputDecoration(
-                    label: TMaker(
-                        text: "Email or Phone",
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                        color: const Color.fromARGB(255, 154, 154, 154)),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                  ),
-                ),
-                PMaker(
-                  top: 20,
-                ),
-                TextField(
-                  onChanged: (value) {
-                    print(value);
-                  },
-                  decoration: InputDecoration(
-                    label: TMaker(
-                        text: "password",
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                        color: const Color.fromARGB(255, 154, 154, 154)),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                  ),
-                ),
-                PMaker(
-                  top: 50,
-                ),
-                MyButton(
-                  text: "Login",
-                  buttonColor: UsedColors.Blue,
-                  onTap: () async {
-                    // SharedPreferences sharedPreferences =
-                    //     await SharedPreferences.getInstance();
-                    // sharedPreferences.setBool("LoggedIn", true);
-                    // Get.toNamed("Home");
-                  },
-                ),
-                PMaker(
-                  top: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+            child: Image.asset(
+              "images/Background.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Column(
+            children: [
+              Expanded(
+                flex: 20,
+                child: CMaker(
+                    alignment: Alignment.center,
+                    child: TMaker(
+                        fontFamily: "Courgette",
+                        text: "Log In",
+                        fontSize: 70,
+                        fontWeight: FontWeight.w600,
+                        color: UsedColors.White)),
+              ),
+              CMaker(
+                color: UsedColors.White,
+                circularRadius: 20,
+                height: 400,
+                width: double.infinity,
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 10,
+                      color: const Color.fromARGB(91, 0, 0, 0),
+                      offset: Offset(2, 2),
+                      spreadRadius: .3)
+                ],
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.all(30),
+                child: Column(
                   children: [
-                    TMaker(
-                        text: "First time ?",
-                        fontSize: 17,
-                        fontWeight: FontWeight.w500,
-                        color: const Color.fromARGB(255, 165, 165, 165)),
                     PMaker(
-                      left: 10,
+                      top: 20,
                     ),
-                    TextButton(
-                        onPressed: () {},
-                        child: TMaker(
-                            text: "Sign Up",
+                    TextField(
+                      onChanged: (value) {
+                        print(value);
+                      },
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.black)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide:
+                                BorderSide(width: 2, color: UsedColors.Blue)),
+                        label: TMaker(
+                            fontFamily: "Courgette",
+                            text: "Email or Phone",
                             fontSize: 20,
                             fontWeight: FontWeight.w500,
-                            color: UsedColors.Blue)),
+                            color: const Color.fromARGB(255, 154, 154, 154)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                      ),
+                    ),
+                    PMaker(
+                      top: 20,
+                    ),
+                    TextField(
+                      onChanged: (value) {
+                        print(value);
+                      },
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.black)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide:
+                                BorderSide(width: 2, color: UsedColors.Blue)),
+                        label: TMaker(
+                            fontFamily: "Courgette",
+                            text: "password",
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            color: const Color.fromARGB(255, 154, 154, 154)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                      ),
+                    ),
+                    PMaker(
+                      top: 50,
+                    ),
+                    MyButton(
+                      textFontFamily: "Courgette",
+                      buttonHeight: 60,
+                      buttonWidth: 140,
+                      textFont: 25,
+                      text: "Login",
+                      buttonColor: UsedColors.Blue,
+                      onTap: () async {
+                        // SharedPreferences sharedPreferences =
+                        //     await SharedPreferences.getInstance();
+                        // sharedPreferences.setBool("LoggedIn", true);
+                        // Get.toNamed("Home");
+                      },
+                    ),
+                    PMaker(
+                      top: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TMaker(
+                            fontFamily: "Courgette",
+                            text: "First time ?",
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500,
+                            color: const Color.fromARGB(255, 165, 165, 165)),
+                        PMaker(
+                          left: 10,
+                        ),
+                        TextButton(
+                            onPressed: () {
+                              Get.offNamed("SignUp");
+                            },
+                            child: TMaker(
+                                fontFamily: "Courgette",
+                                text: "Sign Up",
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                color: UsedColors.Blue)),
+                      ],
+                    ),
                   ],
                 ),
-              ],
-            ),
-          ),
-          Expanded(flex: 2, child: Container()),
-          CMaker(
-            color: UsedColors.White,
-            circularRadius: 20,
-            height: 150,
-            width: double.infinity,
-            boxShadow: [
-              BoxShadow(
-                  blurRadius: 10,
-                  color: const Color.fromARGB(91, 0, 0, 0),
-                  offset: Offset(2, 2),
-                  spreadRadius: .3)
-            ],
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            padding: EdgeInsets.only(top: 15),
-            child: Column(
-              children: [
-                TMaker(
-                    text: "Our Social Media",
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: const Color.fromARGB(255, 171, 171, 171)),
-                Expanded(child: CMaker()),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+              ),
+              Expanded(flex: 2, child: Container()),
+              CMaker(
+                color: UsedColors.White,
+                circularRadius: 20,
+                height: 150,
+                width: double.infinity,
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 10,
+                      color: const Color.fromARGB(91, 0, 0, 0),
+                      offset: Offset(2, 2),
+                      spreadRadius: .3)
+                ],
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.only(top: 15),
+                child: Column(
                   children: [
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.facebook,
-                          size: 60,
-                          color: UsedColors.Blue,
-                        )),
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.tiktok,
-                          size: 60,
-                          color: UsedColors.Blue,
-                        )),
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.tiktok,
-                          size: 60,
-                          color: UsedColors.Blue,
-                        )),
+                    TMaker(
+                        fontFamily: "Courgette",
+                        text: "Our Social Media",
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: const Color.fromARGB(255, 171, 171, 171)),
+                    Expanded(child: CMaker()),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: CMaker(
+                                height: 70,
+                                width: 70,
+                                child: Image.asset("images/5968764.png"))),
+                        IconButton(
+                            onPressed: () {},
+                            icon: CMaker(
+                                height: 70,
+                                width: 70,
+                                child: Image.asset("images/15707749.png"))),
+                        IconButton(
+                            onPressed: () {},
+                            icon: CMaker(
+                                height: 70,
+                                width: 70,
+                                child: Image.asset("images/4494497.png"))),
+                      ],
+                    ),
+                    Expanded(child: CMaker()),
                   ],
                 ),
-                Expanded(child: CMaker()),
-              ],
-            ),
+              ),
+              Expanded(flex: 6, child: Container()),
+            ],
           ),
-          Expanded(flex: 6, child: Container()),
         ],
       ),
     );
