bfb453a6e2d4abfa1cde899f0c00651421eba5f2
hagermohmed <165300040+hagermohmed@users.noreply.github.com>
2024-10-11 20:20:52 +0300

    Done home page

--- codeveloper_to_do/lib/Constants/ColorsUsded.dart
+++ codeveloper_to_do/lib/Constants/ColorsUsded.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class UsedColors {
   static Color White = Colors.white;
   static Color Blue = Color.fromARGB(255, 25, 128, 213);
-  static Color lightGray = Color.fromARGB(255,224,224,224);
-  static Color Orange = Color.fromARGB(255,255,158,66);
-  static Color SoftYello = Color.fromARGB(255,245,230,99);
+  static Color lightGray = Color.fromARGB(255, 224, 224, 224);
+  static Color Orange = Color.fromARGB(255, 255, 158, 66);
+  static Color SoftYello = Color.fromARGB(255, 245, 230, 99);
+  static Color Brown = Color.fromRGBO(91, 87, 47, 1);
 }
--- codeveloper_to_do/lib/View/Pages/PageOne/PageOne.dart
+++ codeveloper_to_do/lib/View/Pages/PageOne/PageOne.dart
@@ -1,13 +1,7 @@
-import 'package:codeveloper_to_do/Constants/ColorsUsded.dart';
 import 'package:codeveloper_to_do/MyTools.dart';
-import 'package:codeveloper_to_do/View/Tasks/DatedTasks/DatedTask.dart';
-import 'package:codeveloper_to_do/View/Tasks/NoDateTasks/NoDateTask.dart';
-import 'package:codeveloper_to_do/data/SignUpData.dart';
 import 'package:codeveloper_to_do/data/Tasks/SpecificTasks/TaskWithDataToDo.dart';
 import 'package:codeveloper_to_do/data/Tasks/Task.dart';
-import 'package:codeveloper_to_do/main.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class PageOne extends StatefulWidget {
   const PageOne({super.key});
@@ -21,33 +15,126 @@ class _PageOneState extends State<PageOne> {
   TaskWithDataToDo taskWithDataToDo = new TaskWithDataToDo();
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        PMaker(
-          top: 40,
+    return ListView(children: [
+      PMaker(top: 50),
+      CMaker(
+        color: const Color.fromARGB(115, 96, 85, 85),
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        circularRadius: 20,
+        child: Row(
+          children: [
+            TMaker(
+                text: "Done",
+                fontSize: 50,
+                fontWeight: FontWeight.w300,
+                color: const Color.fromARGB(255, 250, 107, 107)),
+            TMaker(
+                text: "3/6",
+                fontSize: 50,
+                fontWeight: FontWeight.w300,
+                color: const Color.fromARGB(255, 154, 35, 35)),
+            PMaker(top: 75),
+          ],
         ),
-        CMaker(
+      ),
+      PMaker(
+        top: 10,
+        left: 5,
+      ),
+      CMaker(
+        padding: EdgeInsets.only(right: 150),
+        width: double.infinity,
+        height: 100,
+        color: const Color.fromARGB(255, 255, 255, 255),
+        child: CMaker(
+          alignment: Alignment.center,
+          circularRadius: 20,
           child: TMaker(
-            text: "Hello ",
+            text: "filter",
             fontSize: 50,
-            fontWeight: FontWeight.w600,
-            color: UsedColors.Blue,
-            fontFamily: "Courgette",
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
           ),
+          color: const Color.fromARGB(115, 96, 85, 85),
+          margin: EdgeInsets.symmetric(horizontal: 50),
+          height: 75,
         ),
-        CMaker(
-          child: TMaker(
-            text: "kareem", //"${UserData![0].split(" ")[0]} !!",
-            fontSize: 50,
-            fontWeight: FontWeight.w600,
-            color: UsedColors.Blue,
-            fontFamily: "Courgette",
-          ),
-        ),
-        PMaker(
-          top: 20,
-        )
-      ],
-    );
+        alignment: Alignment.centerLeft,
+      ),
+      PMaker(top: 50),
+      Container(
+          height: 900,
+          child: ListView.builder(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: false,
+            itemCount: 1,
+            itemBuilder: (context, index) {
+              return CMaker(
+                  color: const Color.fromARGB(255, 36, 255, 200),
+                  circularRadius: 20,
+                  margin:
+                      EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
+                  height: 100,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: CMaker(
+                          height: 100,
+                          child: Column(
+                            children: [
+                              CMaker(
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: CMaker(
+                                        height: 50,
+                                        child: Text("Hager"),
+                                        alignment: Alignment.center,
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: CMaker(
+                                        alignment: Alignment.center,
+                                        height: 50,
+                                        child: Text("Hager"),
+                                      ),
+                                    ),
+                                    Expanded(
+                                        child: CMaker(
+                                      alignment: Alignment.center,
+                                      height: 50,
+                                      child: Text("Hager"),
+                                    )),
+                                  ],
+                                ),
+                              ),
+                              CMaker(
+                                height: 50,
+                                padding: EdgeInsets.only(left: 25),
+                                alignment: Alignment.centerLeft,
+                                width: double.infinity,
+                                child: Text("Hager"),
+                                //  color: const Color.fromARGB(5, 90, 106, 102),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                          child: CMaker(
+                        height: 100,
+                        alignment: Alignment.center,
+                        child: Checkbox(
+                          value: true,
+                          onChanged: (value) {},
+                        ),
+                      )),
+                    ],
+                  ));
+            },
+          )),
+    ]);
   }
 }
